561abaa4c610e2e91c3775df1cef93830811f8f6
毛宜甜 <yitian.mao@hand-china.com>
2019-10-16 21:21:06 +0800

    内存测试

---
+++ .history/example/20191016/v8test_20191016211839.js
@@ -0,0 +1,18 @@
+function getme(){
+    var mem = process.memoryUsage();
+    var format = function(bytes){
+        return (bytes/1024/1024).toFixed(2)+'MB';
+    }
+    console.log('heapTotal:'+format(mem.heapTotal)+'heapUsed:'+format(mem.heapUsed));
+}
+var size = 20*1024*1024;
+size=0;
+var arr1 = new Array(size);
+var arr2 = new Array(size);
+var arr3 = new Array(size);
+var arr4 = new Array(size);
+var arr5 = new Array(size);
+var arr6 = new Array(size);
+var arr7 = new Array(size);
+var arr8 = new Array(size);
+getme();
